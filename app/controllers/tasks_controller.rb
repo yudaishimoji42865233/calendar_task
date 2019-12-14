570b1e66bc06fa0ad09c4b71d80814fc77decf51
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2019-12-14 17:44:16 +0900

    view user_name

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -69,6 +69,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content).merge(user_id: current_user.id)
     end
 end
