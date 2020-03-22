f6e767a119923b9e0dc7148acc3a921e02338eba
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2020-03-22 12:20:09 +0900

    add basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
-
+  before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    added_attrs = [:name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
+  end
+
+  private
+
+  def production?
+    Rails.application.credentials.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic_auth[:basic_auth_user] && password == Rails.application.credentials.basic_auth[:basic_auth_password]
+    end
   end
 
 end
